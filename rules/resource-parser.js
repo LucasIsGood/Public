15afee09cc004a30f1d388bef1e18e291649ef1f
LucasIsGood <56297860+LucasIsGood@users.noreply.github.com>
2024-02-15 20:35:22 +0800

    Update resource-parser.js

--- rules/resource-parser.js
+++ rules/resource-parser.js
@@ -1,13 +1,3 @@
-
-/**
-* 使用说明，
-0️⃣ 在QuantumultX 配置文件中[general] 部分，加入 
-resource_parser_url = https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
-⚠️⚠️如提示"没有自定义解析器"，请长按右下角图标后点击左侧刷新按钮，更新资源，后台退出 app，直到出现解析器说明
-
-------------------------------
-*/
-
 //beginning 解析器正常使用，调试注释此部分
 
 let [link0, content0, subinfo] = [$resource.link, $resource.content, $resource.info]
